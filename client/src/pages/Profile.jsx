fccb60bda370444131ccc13c5c4bfbb978447729
Aneesh <aneeshappid@gmail.com>
2024-12-29 22:44:40 +0530

    Added Delete User Functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,6 +1,6 @@
 import {useSelector} from 'react-redux';
 import { useState } from 'react';
-import { updateUserFailure, updateUserStart, updateUserSuccess } from '../redux/user/userSlice.js';
+import { deleteUserSuccess, deleteUserFailure, deleteUserStart, updateUserFailure, updateUserStart, updateUserSuccess } from '../redux/user/userSlice.js';
 import { useDispatch } from 'react-redux';
 
 export default function Profile() {
@@ -38,6 +38,27 @@ export default function Profile() {
       dispatch(updateUserFailure(error.message));
     }
   }
+
+  const handleDeleteUser = async () => {
+    try{
+        dispatch(deleteUserStart());
+        const res = await fetch(`/api/user/delete/${currentUser._id}`, {
+          method: 'DELETE',
+        });
+
+        const data = await res.json();
+
+        if(data.success === false){
+          dispatch(deleteUserFailure(data.message));
+          return;
+        }
+
+        dispatch(deleteUserSuccess(data));
+    } catch(error){
+      dispatch(deleteUserFailure(error.message));
+    }
+  }
+
   return (
     <div className='max-w-3xl mx-auto'>
       <h1 className='text-3xl font-semibold text-center my-7'>Profile</h1>
@@ -71,7 +92,7 @@ export default function Profile() {
         <button disabled={loading} className='bg-slate-800 text-white rounded-lg p-3 uppercase hover:opacity-90 disabled:opacity-80'>{loading? 'Loading...' : "Update"}</button>
       </form>
       <div className='flex justify-between mt-5'>
-        <span className='text-red-600 cursor-pointer'>Delete account</span>
+        <span onClick={handleDeleteUser} className='text-red-600 cursor-pointer'>Delete account</span>
         <span className='text-red-600 cursor-pointer'>Sign out</span>
       </div>
       <p className='text-red-700 mt-5 text-xl text-center font-semibold'>{error? error: ''}</p>
