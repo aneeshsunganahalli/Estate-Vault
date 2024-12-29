b09aafc15807102b783fa1c54442961fd8757b24
Aneesh <aneeshappid@gmail.com>
2024-12-29 22:30:11 +0530

    Completed User Update Functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,22 +1,81 @@
 import {useSelector} from 'react-redux';
+import { useState } from 'react';
+import { updateUserFailure, updateUserStart, updateUserSuccess } from '../redux/user/userSlice.js';
+import { useDispatch } from 'react-redux';
 
 export default function Profile() {
-  const {currentUser} = useSelector(state => state.user);
+  const dispatch = useDispatch();
+  const {currentUser, loading, error} = useSelector(state => state.user);
+  const [formData, setFormData] = useState({});
+  const [updateSuccess, setUpdateSuccess] = useState(false);
+
+  const handleChange = (e) => {
+    setFormData({...formData, [e.target.id]: e.target.value});
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      dispatch(updateUserStart());
+      const res = await fetch(`/api/user/update/${currentUser._id}`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+      });
+
+      const data = await res.json();
+
+      if(data.success === false){
+        dispatch(updateUserFailure(data.message));
+        return;
+      }
+
+      dispatch(updateUserSuccess(data));
+      setUpdateSuccess(true);
+    } catch(error){
+      dispatch(updateUserFailure(error.message));
+    }
+  }
   return (
     <div className='max-w-3xl mx-auto'>
       <h1 className='text-3xl font-semibold text-center my-7'>Profile</h1>
-      <form className='flex flex-col gap-5'>
+      <form onSubmit={handleSubmit} className='flex flex-col gap-5'>
         <img src={currentUser.avatar} alt="Profile" className='rounded-full h-24 w-24 object-cover self-center cursor-pointer mt-2' />
-        <input type='text' placeholder='Username' className='border p-3 rounded-lg' id='username'/>
-        <input type='text' placeholder='Email' className='border p-3 rounded-lg' id='email'/>
-        <input type='text' placeholder='Password' className='border p-3 rounded-lg' id='password'/>
 
-        <button className='bg-slate-800 text-white rounded-lg p-3 uppercase hover:opacity-90 disabled:opacity-80'>Update</button>
+        <input 
+          type='text' 
+          placeholder='Username' 
+          className='border p-3 rounded-lg' 
+          id='username' 
+          defaultValue={currentUser.username}
+          onChange={handleChange}
+        />
+        <input 
+          type='email' 
+          placeholder='Email' 
+          className='border p-3 rounded-lg' 
+          id='email' 
+          defaultValue={currentUser.email}
+          onChange={handleChange}
+        />
+        <input 
+          type='password' 
+          placeholder='Password' 
+          className='border p-3 rounded-lg' 
+          id='password'
+          onChange={handleChange}
+        />
+
+        <button disabled={loading} className='bg-slate-800 text-white rounded-lg p-3 uppercase hover:opacity-90 disabled:opacity-80'>{loading? 'Loading...' : "Update"}</button>
       </form>
       <div className='flex justify-between mt-5'>
         <span className='text-red-600 cursor-pointer'>Delete account</span>
         <span className='text-red-600 cursor-pointer'>Sign out</span>
       </div>
+      <p className='text-red-700 mt-5 text-xl text-center font-semibold'>{error? error: ''}</p>
+      <p className='text-green-600 mt-5 text-xl text-center font-semibold'>{updateSuccess ? 'User updated successfully' : '' }</p>
     </div>
   )
 }
